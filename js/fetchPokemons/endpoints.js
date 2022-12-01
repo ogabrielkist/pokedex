1994dfa533cb50e9c17dcbd922f2f8f730ce8bc7
jose iedo <iedoduarte1@gmail.com>
2022-12-01 11:27:19 -0300

    funções principais da pokedex

--- js/fetchPokemons/endpoints.js
+++ js/fetchPokemons/endpoints.js
@@ -1,16 +1,6 @@
-async function searchPokemonById(id) {
-  const data = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
-
-  const json = await data.json();
-
-  return json;
-}
-
 async function searchAllPokemons() {
   const data = await fetch("https://pokeapi.co/api/v2/pokemon/");
-
   const json = await data.json();
-  // console.log(json);
   return json;
 }
 
@@ -24,6 +14,9 @@ async function getMoreData(currentDataCallback) {
 }
 
 async function handlePokemonList() {
+  const list = document.querySelector("#list");
+  list.innerHTML = "";
+
   let currentDataPage = await searchAllPokemons();
 
   addPokemonsInThePage(currentDataPage);
@@ -33,8 +26,6 @@ async function handlePokemonList() {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
     if (scrollTop + clientHeight >= scrollHeight - 5 && !wait) {
-      console.log("uga");
-
       wait = true;
       setTimeout(async () => {
         wait = false;
@@ -53,8 +44,82 @@ async function addPokemonsInThePage({ results }) {
   results.forEach(async ({ name, url }) => {
     const data = await fetch(url);
     const json = await data.json();
-    list.innerHTML += `<li> <img src="${json.sprites.front_default}" alt=""><span>${name}</span></li>`;
+
+    list.innerHTML += `<li onclick="sendDataOfPokemonToModal(event)"> 
+                        <img src="${json.sprites.front_default}" alt="">
+                        <span>${name}</span>
+                      </li>`;
   });
 }
+
+async function handlePokemonSearch(event) {
+  event.preventDefault();
+
+  const typedPokemon = document.querySelector(".searchbar").value.toLowerCase();
+  const error = document.querySelector(".error");
+
+  if (!typedPokemon.length) {
+    error.innerText =
+      "Hey! you forgot to insert the name of the pokemon you want.";
+    handlePokemonList();
+    return;
+  }
+
+  try {
+    const data = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/" + typedPokemon
+    );
+    json = await data.json();
+
+    showSinglePokemonCard(json);
+  } catch (e) {
+    error.innerText = `Error: Pokemon with the name "${typedPokemon}" not found`;
+  }
+}
+
+function removeErrorMessage() {
+  const error = document.querySelector(".error");
+
+  error.innerText = "";
+}
+
+function showSinglePokemonCard(data) {
+  const list = document.querySelector("#list");
+  list.innerHTML = `<li onclick="sendDataOfPokemonToModal(event)"> 
+                     <img src="${data.sprites.front_default}" alt="">
+                     <span>${data.name}</span>
+                    </li>`;
+
+  const showAllbtn = document.querySelector(".showall");
+  showAllbtn.style.display = "block";
+}
+
+function sendDataOfPokemonToModal(event) {
+  const pokeValue = event.currentTarget.querySelector("span").innerText;
+  console.log(pokeValue);
+}
+
+const form = document.querySelector(".searchform");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  handlePokemonSearch(event);
+});
+
+const btn = document.querySelector(".searchbarbtn");
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  handlePokemonSearch(event);
+});
+
+const showAllbtn = document.querySelector(".showall");
+showAllbtn.addEventListener("click", async () => {
+  showAllbtn.style.display = "none";
+
+  let currentDataPage = await searchAllPokemons();
+  const list = document.querySelector("#list");
+  list.innerHTML = "";
+
+  addPokemonsInThePage(currentDataPage);
+});
 
 handlePokemonList();
